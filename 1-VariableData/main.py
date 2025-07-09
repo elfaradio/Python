d1cff4b01efb86aa4971aa02e38fd0622cf6abff
Farhadul Islam <farhadulislam84054@gmail.com>
2025-07-09 21:57:09 +0600

    first

---
+++ 1-VariableData/main.py
@@ -0,0 +1,10 @@
+print("Max Verstappen is the best F1 driver of all time!")
+a = 28  # integer
+b = 3.14  # float
+s = "Farhad"  # string
+c = 'a'  # character
+d = True  # boolean
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
